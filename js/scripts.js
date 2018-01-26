cc3f953bc4755c96aececac89193a9b00c19cd3a
Agnieszka Nowacka <aguo84@gmail.com>
2018-01-26 22:44:22 +0100

    Add scripts

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;	
+		this.$element = $('<button>');
+		this.$element.text(this.text).css('padding', '15px').css('background-color', '#4aa86b').css('color', '#333').css('fontSize', '20px');
+		this.$element.on('click', function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element); //alternatywnie: this.$element.appendTo($('body'));
+	}
+}
+
+var btn1 = new Button('Welcome!');
+btn1.create();
